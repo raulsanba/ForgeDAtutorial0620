3ed70d14cb3a66988da958888bbf5101ea6a7567
raulsanba <nogasanrl@gmail.com>
2020-06-16 12:33:36 +0100

    Define Activity error identified. Qualified ID for AppBundle and activity working OK.

--- ForgeTutorialDA040620/Controllers/DesignAutomationController.cs
+++ ForgeTutorialDA040620/Controllers/DesignAutomationController.cs
@@ -14,6 +14,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Activity = Autodesk.Forge.DesignAutomation.Model.Activity;
 using Alias = Autodesk.Forge.DesignAutomation.Model.Alias;
@@ -33,16 +34,19 @@ namespace ForgeTutorialDA040620.Controllers
         // used to access the SignalR Hub
         private IHubContext<DesignAutomationHub> _hubContext;
         // Local folder for bundles
-        public string LocalBundlesFolder { 
-            get { return Path.Combine(_env.WebRootPath, "bundles"); } 
+        public string LocalBundlesFolder
+        {
+            get { return Path.Combine(_env.WebRootPath, "bundles"); }
         }
         /// Prefix for AppBundles and Activities
-        public static string NickName { 
-            get { return oAuthController.GetAppSetting("FORGE_CLIENT_ID"); } 
+        public static string NickName
+        {
+            get { return oAuthController.GetAppSetting("FORGE_CLIENT_ID"); }
         }
         /// Alias for the app (e.g. DEV, STG, PROD). This value may come from an environment variable
-        public static string Alias { 
-            get { return "dev"; } 
+        public static string Alias
+        {
+            get { return "dev"; }
         }
         // Design Automation v3 API
         DesignAutomationClient _designAutomation;
@@ -55,12 +59,13 @@ namespace ForgeTutorialDA040620.Controllers
             _hubContext = hubContext;
         }
 
+
         // **********************************
         //
         // Next we will add the methods here
         //
         // **********************************
-        
+
         //Method for GetLocalBundles->Look at the bundles folder and return a list of .ZIP files
 
         [HttpGet]
@@ -87,7 +92,7 @@ namespace ForgeTutorialDA040620.Controllers
         //Method to define a new AppBundle
         [HttpPost]
         [Route("api/forge/designautomation/appbundles")]
-        public async Task<IActionResult> CreateAppBundle([FromBody]JObject appBundleSpecs)
+        public async Task<IActionResult> CreateAppBundle([FromBody] JObject appBundleSpecs)
         {
             //input validation 
 
@@ -100,7 +105,7 @@ namespace ForgeTutorialDA040620.Controllers
 
             //check if ZIP with bundle is here
             string packageZipPath = Path.Combine(LocalBundlesFolder, zipFileName + ".zip");
-            if(!System.IO.File.Exists(packageZipPath))
+            if (!System.IO.File.Exists(packageZipPath))
             {
                 throw new Exception("AppBundle not found at " + packageZipPath);
             }
@@ -111,7 +116,7 @@ namespace ForgeTutorialDA040620.Controllers
             //check if AppBundle is already defined
             dynamic newAppVersion;
             string qualifiedAppBundleId = string.Format("{0}.{1}+{2}", NickName, appBundleName, Alias);
-            if(!appBundles.Data.Contains(qualifiedAppBundleId))
+            if (!appBundles.Data.Contains(qualifiedAppBundleId))
             {
                 //create an appbundle in version 1
                 AppBundle appBundleSpec = new AppBundle()
@@ -122,7 +127,7 @@ namespace ForgeTutorialDA040620.Controllers
                     Description = string.Format("Description for {0}", appBundleName)
                 };
                 newAppVersion = await _designAutomation.CreateAppBundleAsync(appBundleSpec);
-                if(newAppVersion == null)
+                if (newAppVersion == null)
                 {
                     throw new Exception("Cannot create new app");
                 }
@@ -132,6 +137,7 @@ namespace ForgeTutorialDA040620.Controllers
                     Id = Alias,
                     Version = 1
                 };
+                Alias newAlias = await _designAutomation.CreateAppBundleAliasAsync(appBundleName, aliasSpec);
 
             }
             else
@@ -143,7 +149,7 @@ namespace ForgeTutorialDA040620.Controllers
                     Description = appBundleName
                 };
                 newAppVersion = await _designAutomation.CreateAppBundleVersionAsync(appBundleName, appBundleSpec);
-                if(newAppVersion == null)
+                if (newAppVersion == null)
                 {
                     throw new Exception("Cannot create new version");
                 }
@@ -161,6 +167,7 @@ namespace ForgeTutorialDA040620.Controllers
             request.AlwaysMultipartFormData = true;
             foreach (KeyValuePair<string, string> x in newAppVersion.UploadParameters.FormData) request.AddParameter(x.Key, x.Value);
             request.AddFile("file", packageZipPath);
+            request.AddHeader("Cache-Control", "no-cache");
             await uploadClient.ExecuteTaskAsync(request);
 
             return Ok(new
@@ -168,16 +175,111 @@ namespace ForgeTutorialDA040620.Controllers
                 AppBundle = qualifiedAppBundleId,
                 Version = newAppVersion.Version
             });
+
+
         }
+
+        //Define Activity Methods
+        //Identify engine. I will congifure Inventor only
+        private dynamic EngineAttributes(string engine)
+        {
+            if (engine.Contains("3dsMax")) return new { commandLine = @"$(engine.path)\\3dsmaxbatch.exe -sceneFile $(args[inputFile].path) $(settings[script].path)", extension = "max", script = "da = dotNetClass(\"Autodesk.Forge.Sample.DesignAutomation.Max.RuntimeExecute\")\nda.ModifyWindowWidthHeight()\n" };
+            if (engine.Contains("AutoCAD")) return new { commandLine = "$(engine.path)\\accoreconsole.exe /i $(args[inputFile].path) /al $(appbundles[{0}].path) /s $(settings[script].path)", extension = "dwg", script = "UpdateParam\n" };
+            if (engine.Contains("Inventor")) return new { commandLine = "$(engine.path)\\InventorCoreConsole.exe /i $(args[inputFile].path) /al $(appbundles[{0}].path)", extension = "ipt", script = string.Empty };
+            if (engine.Contains("Revit")) return new { commandLine = "$(engine.path)\\revitcoreconsole.exe /i $(args[inputFile].path) /al $(appbundles[{0}].path)", extension = "rvt", script = string.Empty };
+            throw new Exception("Invalid engine");
+        }
+
+        //Define a new Activity
+        [HttpPost]
+        [Route("api/forge/designautomation/activities")]
+        public async Task<IActionResult> CreateActivity([FromBody] JObject activitySpecs)
+        {
+            //input validation
+            string zipFileName = activitySpecs["zipFileName"].Value<string>();
+            string engineName = activitySpecs["engine"].Value<string>();
+
+            // standard name for this sample
+            string appBundleName = zipFileName + "AppBundle";
+            string activityName = zipFileName + "Activity";
+
+            Page<string> activities = await _designAutomation.GetActivitiesAsync();
+            string qualifiedActivityId = string.Format("{0}.{1}+{2}", NickName, activityName, Alias);
+            if (!activities.Data.Contains(qualifiedActivityId))
+            {
+                //define new activity
+                dynamic engineAttributes = EngineAttributes(engineName);
+                string commandLine = string.Format(engineAttributes.commandLine, appBundleName);
+                Activity activitySpec = new Activity()
+                {
+                    Id = activityName,
+                    Appbundles = new List<string>() { string.Format("{0}.{1}+{2}", NickName, appBundleName, Alias) },
+                    CommandLine = new List<string>() { commandLine },
+                    Engine = engineName,
+                    Parameters = new Dictionary<string, Parameter>()
+                    {
+                        { "inputFile", new Parameter() { Description = "input file", LocalName = "$(inputFile)", Ondemand = false, Required = true, Verb = Verb.Get, Zip = false } },
+                        { "inputJson", new Parameter() { Description = "input Json", LocalName = "params.json", Ondemand = false, Required = false, Verb = Verb.Get, Zip = false } },
+                        { "outputFile", new Parameter() { Description = "output file", LocalName = "outputFile." + engineAttributes.extension, Ondemand = false, Required = true, Verb = Verb.Put, Zip = false } }
+                    },
+                    Settings = new Dictionary<string, ISetting>()
+                    {
+                        { "script", new StringSetting() { Value = engineAttributes.script } }
+                    }
+                };
+                Activity newActivity = await _designAutomation.CreateActivityAsync(activitySpec);
+
+                //specify Alias for this activity
+                Alias aliasSpec = new Alias() { Id = Alias, Version = 1 };
+                Alias newAlias = await _designAutomation.CreateActivityAliasAsync(activityName, aliasSpec);
+
+                return Ok(new { Activity = qualifiedActivityId });
+            }
+            // as this activity points to a AppBundle "dev" alias (which points to the last version of the bundle),
+            // there is no need to update it (for this sample), but this may be extended for different contexts
+            return Ok(new { Activity = "Activity already defined" });
+        }
+
+
+
+        //Method to return all defined activities.It will only return activities defined by Client ID
+
+        //Get all activities defined for this account
+        [HttpGet]
+        [Route("api/forge/designautomation/activities")]
+        public async Task<List<string>> GetDefinedActivities()
+        {
+            //filter list of
+            Page<string> activities = await _designAutomation.GetActivitiesAsync();
+            List<string> definedActivities = new List<string>();
+            foreach (string activity in activities.Data)
+                if (activity.StartsWith(NickName) && activity.IndexOf("$LATEST") == -1)
+                {
+                    definedActivities.Add(activity.Replace(NickName + ".", String.Empty));
+                }
+
+            return definedActivities;
+        }
+
+        [HttpDelete]
+        [Route("api/forge/designautomation/account")]
+        public async Task<IActionResult> ClearAccount()
+        {
+            await _designAutomation.DeleteForgeAppAsync("me");
+            return Ok();
+        }
+
+
     }
 
 
-    /// <summary>
-    /// Class uses for SignalR
-    /// </summary>
-    public class DesignAutomationHub : Microsoft.AspNetCore.SignalR.Hub
-    {
-        public string GetConnectionId() { return Context.ConnectionId; }
-    }
+        /// <summary>
+        /// Class uses for SignalR
+        /// </summary>
+        public class DesignAutomationHub : Microsoft.AspNetCore.SignalR.Hub
+        {
+            public string GetConnectionId() { return Context.ConnectionId; }
+        }
 
+    
 }
